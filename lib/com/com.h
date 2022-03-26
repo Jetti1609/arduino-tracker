accecd04d9bcc794359239f7381dddf1339cd8e9
Jetti1609 <Jetthaichal.Janu@outlook.de>
2022-03-26 10:21:16 +0100

    e

---
+++ lib/com/com.h
@@ -0,0 +1,30 @@
+namespace com{
+    void succ(){
+    while (SerialUSB.available()) {
+    read = SerialUSB.readStringUntil('\n');
+    read.trim();
+
+    if(read = 'DEC1'){
+        step::DE::CW();
+        SerialUSB.print(read);
+    }
+    else if(read = 'DEC2'){
+        step::DE::CCW();
+        SerialUSB.print(read);
+        
+    }
+    else if(read = 'RA1'){
+        step::RA::CW();
+        SerialUSB.print(read);
+    }
+    else if(read = 'RA2'){
+        step::RA::CCW();
+        SerialUSB.print(read);
+    }
+    delay(5);
+    }
+    step::RA::CCW();
+    delay(2700);
+    SerialUSB.print('.');
+}
+}
