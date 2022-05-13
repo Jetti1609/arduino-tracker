e8df2d254cd1f318bc1a6a5b072b859f29efd2eb
Jetti1609 <Jetthaichal.Janu@outlook.de>
2022-05-13 17:25:06 +0200

    s

--- lib/com/com.h
+++ lib/com/com.h
@@ -1,5 +1,7 @@
 String command;
 
+const int stepsperdeg = 89;
+
 namespace com{
     void com(){
     while (Serial.available()) {
@@ -17,6 +19,30 @@ namespace com{
         }
         else if(command.equals("RA2")){
             step::RA::CCW();
+        }
+        else if(command.equals("DEC1G")){
+            for (int i = 1; i <= stepsperdeg; i++){
+            step::DE::CW();
+            delayMicroseconds(100);
+            }
+        }
+        else if(command.equals("DEC2G")){
+            for (int i2 = 1; i2 <= stepsperdeg; i2++){
+            step::DE::CCW();
+            delayMicroseconds(100);
+            }
+        }
+        else if(command.equals("RA1G")){
+            for (int i3 = 1; i3 <= stepsperdeg; i3++){
+            step::RA::CW();
+            delayMicroseconds(100);
+            }
+        }
+        else if(command.equals("RA2G")){
+            for (int i4 = 1; i4 <= stepsperdeg; i4++){
+            step::RA::CCW();
+            delayMicroseconds(100);
+            }
         }
     }
     
--- src/arduino-tracker.cpp
+++ src/arduino-tracker.cpp
@@ -1,16 +1,12 @@
 #include <Arduino.h>
-#include <LiquidCrystal_I2C.h>
 #include <Wire.h>
 #include <motors.h>
 #include <com.h>
 
-LiquidCrystal_I2C lcd(0x27,20,4);
 
 
 void setup(){
     Serial.begin(9600);
-    lcd.init();
-    lcd.init();
     pinMode(STEPPIN_DEC, OUTPUT);
     pinMode(STEPPIN_RA, OUTPUT);
     pinMode(DIRPIN_DEC, OUTPUT);
@@ -22,16 +18,6 @@ void setup(){
     
 void loop(){
     com::com();
-  // Print a message to the LCD.
-  lcd.backlight();
-  lcd.setCursor(0,0);
-  lcd.print("RA:");
-  lcd.setCursor(4,0);
-  lcd.print(RApos1);
-  lcd.setCursor(0,1);
-  lcd.print("DEC:");
-  lcd.setCursor(5,1);
-  lcd.print(DEpos1);
     
 }
 
